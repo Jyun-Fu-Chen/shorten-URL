6f855a96b3f2f89f28ec54cb7de39507928447c2
Jyun-Fu-Chen <than3698741@gmail.com>
2022-03-09 01:27:34 +0800

    feat: add generateShortUrl

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const PORT = 3000
 const express = require('express')
 const app = express()
 const exhbs = require('express-handlebars')
+const generateShortUrl = require('./generateShortUrl')
 const bodyParser = require('body-parser')
 const URL = require('./models/shortUrl')
 const mongoose = require('mongoose')
+const { redirect } = require('statuses')
 mongoose.connect('mongodb://localhost/short-url-list')
 const db = mongoose.connection
 db.on('error', () => {
@@ -25,9 +27,23 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-
-  console.log(req.body.url)
+  const originalUrl = req.body.url
+  URL.find()
+    .then(urls => {
+      let shortUrl = generateShortUrl()
+      if (urls.includes(shortUrl)) {
+        shortUrl = generateShortUrl()
+      } else {
+        URL.create({
+          originalUrl: originalUrl,
+          shortUrl: shortUrl
+        })
+      }
+    })
+    .then(()=>res.redirect('/'))
+    .catch(err => console.log(err))
 })
+
 
 
 app.listen(PORT, () => {
---
+++ generateShortUrl.js
@@ -0,0 +1,19 @@
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCase = upperCase.toLocaleLowerCase()
+const number = "1234567890"
+const generateWords = upperCase + lowerCase + number
+const generateArr = generateWords.split('')
+
+
+const generateShortUrl = function () {
+  let code = " "
+  
+  const randomNumber = generateArr.length + 1
+
+  for (let i = 0; i < 5; i++) {
+    code += generateArr[Math.floor(Math.random() * randomNumber)]
+  }
+  return code
+}
+
+module.exports = generateShortUrl
