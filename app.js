a584669bca89add6c7d14386526380ce1feda8ff
Jyun-Fu-Chen <than3698741@gmail.com>
2022-03-08 14:47:54 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ const PORT = 3000
 const express = require('express')
 const app = express()
 const exhbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
+const URL = require('./models/shortUrl')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/short_url_list')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
 
 app.engine('.hbs', exhbs.engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
   res.render('home')
-
 })
+
+app.post('/', (req, res) => {
+
+  console.log(req.body.url)
+})
+
 
 app.listen(PORT, () => {
   console.log(`localhost:${PORT} is running`)
